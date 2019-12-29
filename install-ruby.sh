ee520a8af023e1f549018f7c26647304d0f1c6ce
ocha <ochaochaocha3@gmail.com>
2019-12-30 01:07:29 +0900

    install-ruby: ジョブ数を9に変更する
    8コア + 1

--- install-ruby.sh
+++ install-ruby.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-MAKEOPTS='-j5' RUBY_CONFIGURE_OPTS='--disable-install-doc --enable-shared' rbenv install --verbose $1
+MAKEOPTS='-j9' RUBY_CONFIGURE_OPTS='--disable-install-doc --enable-shared' rbenv install --verbose $1
