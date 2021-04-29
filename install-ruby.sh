93da4402e56963d726957ec346117902e5609d86
ocha <ochaochaocha3@gmail.com>
2021-04-29 21:06:52 +0900

    install-ruby: CPU数6に合わせる

--- install-ruby.sh
+++ install-ruby.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-MAKEOPTS='-j5' RUBY_CONFIGURE_OPTS='--disable-install-doc --enable-shared' rbenv install --verbose $1
+MAKEOPTS='-j7' RUBY_CONFIGURE_OPTS='--disable-install-doc --enable-shared' rbenv install --verbose $1
